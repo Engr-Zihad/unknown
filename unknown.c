cb5d4949a2824bd037cc9870402825c1abc5c0fe
Engr-Zihad <mojahidulislamzihad686@gmail.com>
2026-04-28 23:43:24 +0600

    new formula

--- unknown.c
+++ unknown.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main(){
+
+void rectangle(){
     double a,b;
     printf("enter two numbers :\n");
     scanf("%lf%lf",&a,&b);
@@ -7,5 +8,24 @@ int main(){
     double perimeter=2*(a+b);
     printf("area of rectegle is :%lf\n",area);
     printf("perimeter of rectegle is :%lf\n",perimeter);
-   return 0;
+}
+
+void average(){
+    double a,b,c;
+    printf("enter three numbers :\n"); 
+    scanf("%lf%lf%lf",&a,&b,&c);
+    double avarage=(a+b+c)/3;
+    printf("avarage of three numbers is :%lf\n",avarage);
+}
+
+int main(){
+    int choice;
+    printf("1. Rectangle\n");
+    printf("2. Average\n");
+    printf("Choice: ");
+    scanf("%d",&choice);
+    if(choice==1) rectangle();
+    else if(choice==2) average();
+    else printf("invalid choice!\n");
+    return 0;
 }
